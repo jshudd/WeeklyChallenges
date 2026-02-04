fc35b03ee029f16e6915a93ad38fec2c4a322da0
jshudd <62891431+jshudd@users.noreply.github.com>
2026-02-04 16:09:08 -0600

    solved AddEvenSubtractOdd

--- ChallengesWithTestsMark8/obj/Debug/net6.0/ChallengesWithTestsMark8.AssemblyInfo.cs
+++ ChallengesWithTestsMark8/obj/Debug/net6.0/ChallengesWithTestsMark8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e4c98b29f0ad42ce5c3b0ca827f96bde8c9226e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac62a591e73408698c4e0e1036e879dc9135fa3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
