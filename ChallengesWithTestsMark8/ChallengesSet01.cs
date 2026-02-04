6d3e2f46d4e37914a201d82f3c6360c4badfecc7
jshudd <62891431+jshudd@users.noreply.github.com>
2026-02-04 09:58:29 -0600

    solved Subtract

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -20,7 +20,7 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend - subtrahend;
         }
 
         public int Add(int number1, int number2)
