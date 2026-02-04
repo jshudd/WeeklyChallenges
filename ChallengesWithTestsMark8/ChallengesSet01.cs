1d5d0a1ad80bfe449bb8148f4fc03929695502e3
jshudd <62891431+jshudd@users.noreply.github.com>
2026-02-04 09:51:16 -0600

    solved 1st

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,14 +6,16 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            if(num1 == num2)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            //if(num1 == num2)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+
+            return num1 == num2 ? true : false;
         }
 
         public double Subtract(double minuend, double subtrahend)
