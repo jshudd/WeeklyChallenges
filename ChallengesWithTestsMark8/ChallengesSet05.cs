3f1e57a434bf9a5c48b5381f91cbf2fa716114bb
jshudd <62891431+jshudd@users.noreply.github.com>
2026-02-05 13:52:32 -0600

    solved TwoDifferentElementsInArrayCanSumToTargetNumber

--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -92,7 +92,15 @@ namespace ChallengesWithTestsMark8
 
         public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] nums, int targetNumber)
         {
-            throw new NotImplementedException();
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = i+1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == targetNumber)
+                        return true;
+                }
+            }
+            return false;
         }
     }
 }
--- ChallengesWithTestsMark8/obj/Debug/net6.0/ChallengesWithTestsMark8.AssemblyInfo.cs
+++ ChallengesWithTestsMark8/obj/Debug/net6.0/ChallengesWithTestsMark8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25a68a750f830b730ee2e2f6bc24932973036e1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f7a149c3e3d83f99b08716203512922c468a3b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
